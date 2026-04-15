d76dae80dc99680a7cf1c30b9a3b4e21485d4918
rejoy2004-rgb <rejoy2004@gmail.com>
2026-04-15 16:27:34 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import json
-from main import final
-
-#Set title of the Streamlit app
-st.title("Conversation Evaluator")
-
-#Display evaluation results in a JSON format
-st.json(final)
